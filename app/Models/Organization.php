2f2a27b5043fa9c07ee204f42b2c01e652695a1f
Cauê Assmann Silva <caueassmannsilva114@gmail.com>
2025-04-12 16:51:26 -0300

    feat: organizations with encryption

---
+++ app/Models/Organization.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\HasEncrypt;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Organization extends Model
+{
+    use HasUuids,HasFactory, HasEncrypt;
+
+    protected $fillable = [ "name", "name_index"];
+
+    protected function name(): Attribute
+    {
+        return Attribute::make(...$this->makeEncryptedAttributeCallables('name'));
+    } 
+
+    protected static function booted()
+    {
+        static::saving(function (Organization $organization) {
+            $organization->encryptColumnIndex("name", "name_index");
+        });
+    }
+}
