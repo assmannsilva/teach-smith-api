79236342fcfbf2301cc0c1862ba22f6a0457cb20
Cauê Assmann Silva <caueassmannsilva114@gmail.com>
2025-04-30 07:07:16 -0300

    refat: userservice dir

--- app/Http/Controllers/Users/RegisterAdminUserController.php
+++ app/Http/Controllers/Users/RegisterAdminUserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Users\RegisterAdminUserRequest;
-use App\Services\UserService;
+use App\Services\User\UserService;
 
 class RegisterAdminUserController extends Controller
 {
