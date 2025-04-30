b637daccb51574c4f467d6c3c38cb786bf6be7a2
Cauê Assmann Silva <caueassmannsilva114@gmail.com>
2025-04-30 07:10:16 -0300

    refat: base repository functions

--- app/Repositories/BaseRepository.php
+++ app/Repositories/BaseRepository.php
@@ -9,66 +9,79 @@ use Illuminate\Pagination\AbstractPaginator as Paginator;
 
 abstract class BaseRepository
 {
-  /**
-   * Model class for repo.
-   *
-   * @var string
-   */
-  protected $modelClass;
+	/**
+	 * Model class for repo.
+	 *
+	 * @var string
+	 */
+	protected $modelClass;
 
-  /**
-   * create a instance of the builder
-   * @return EloquentQueryBuilder|QueryBuilder
-   */
-  protected function newQuery()
-  {
-    return app($this->modelClass)->newQuery();
-  }
+	/**
+	 * create a instance of the builder
+	 * @return EloquentQueryBuilder|QueryBuilder
+	 */
+	protected function newQuery()
+	{
+		return app($this->modelClass)->newQuery();
+	}
 
-  /**
-   * Executes the repository query
-   * @param EloquentQueryBuilder|QueryBuilder $query
-   * @param int                               $take
-   * @param bool                              $paginate
-   * @return EloquentCollection|Paginator
-   */
-  protected function doQuery($query = null, $take = 15, $paginate = true)
-  {
-    if (is_null($query)) $query = $this->newQuery();
+	/**
+	 * Executes the repository query
+	 * @param EloquentQueryBuilder|QueryBuilder $query
+	 * @param int                               $take
+	 * @param bool                              $paginate
+	 * @return EloquentCollection|Paginator
+	 */
+	protected function doQuery($query = null, $take = 15, $paginate = true)
+	{
+		if (is_null($query)) $query = $this->newQuery();
 
-    if ($paginate) return $query->paginate($take);
-    
-    if ($take > 0 || false !== $take) $query->take($take);
-    
-    return $query->get();
-  }
+		if ($paginate) return $query->paginate($take);
+		
+		if ($take > 0 || false !== $take) $query->take($take);
+		
+		return $query->get();
+	}
 
-  /**
-   * Returns all records.
-   * If $take is false then brings all records
-   * If $paginate is true returns Paginator instance.
-   *
-   * @param int  $take
-   * @param bool $paginate
-   *
-   * @return EloquentCollection|Paginator
-   */
-  protected function getAll($take = 15, $paginate = true)
-  {
-    return $this->doQuery(null, $take, $paginate);
-  }
+	/**
+	 * Returns all records.
+	 * If $take is false then brings all records
+	 * If $paginate is true returns Paginator instance.
+	 *
+	 * @param int  $take
+	 * @param bool $paginate
+	 *
+	 * @return EloquentCollection|Paginator
+	 */
+	protected function getAll($take = 15, $paginate = true)
+	{
+		return $this->doQuery(null, $take, $paginate);
+	}
 
 
-  /**
-   * Retrieves a record by his id
-   * @param string  $id
-   * @param bool $fail
-   * @return mixed
-   */
-  protected function findById(String $id, bool $fail = true)
-  {
-    if ($fail) return $this->newQuery()->findOrFail($id);
-    
-    return $this->newQuery()->find($id);
-  }
+	/**
+	 * Retrieves a record by his id
+	 * @param string  $id
+	 * @param bool $fail
+	 * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+	 * @return mixed
+	 */
+	public function find(String $id, bool $fail = true)
+	{
+		if ($fail) return $this->newQuery()->findOrFail($id);
+		
+		return $this->newQuery()->find($id);
+	}
+
+	/**
+	 * Creates a new record
+	 * @param string  $id
+	 * @param bool $fail
+	 * @return mixed
+	 */
+	public function create(Array $insert_data)
+	{
+		return $this->modelClass::create($insert_data);
+	}
+
 }
--- app/Repositories/OrganizationRepository.php
+++ app/Repositories/OrganizationRepository.php
@@ -8,26 +8,4 @@ use App\Repositories\BaseRepository;
 class OrganizationRepository extends BaseRepository implements OrganizationRepositoryInterface {
 
     protected $modelClass = Organization::class;
-
-    /**
-     * Find an organization by its ID.
-     * @param String $id
-     * @return Organization
-     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
-     */
-    public function find(String $id) : Organization
-    {
-        return $this->findById($id);
-    }
-
-    /**
-     * Creates a new Organization
-     * @param Array $insert_data
-     * @return Organization
-     */
-    public function create(Array $insert_data) : Organization
-    {
-        return Organization::create($insert_data);
-    }
-
 }
