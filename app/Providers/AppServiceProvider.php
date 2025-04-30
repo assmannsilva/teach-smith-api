6e15f9811d8f516d43316cf2404c5a7dc4fdd99e
Cauê Assmann Silva <caueassmannsilva114@gmail.com>
2025-04-30 07:13:07 -0300

    feat: bind interfaces

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,11 @@
 namespace App\Providers;
 
 use App\Repositories\Interfaces\OrganizationRepositoryInterface;
+use App\Repositories\Interfaces\TeacherRepositoryInterface;
+use App\Repositories\Interfaces\UserRepositoryInterface;
 use App\Repositories\OrganizationRepository;
+use App\Repositories\TeacherRepository;
+use App\Repositories\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +18,8 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(OrganizationRepositoryInterface::class, OrganizationRepository::class);
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(TeacherRepositoryInterface::class, TeacherRepository::class);
     }
 
     /**
