f3b4f32ea3282aea47710e49f75a0618cde2a299
Cauê Assmann Silva <caueassmannsilva114@gmail.com>
2025-05-01 12:21:46 -0300

    feat: students model

---
+++ app/Models/Student.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasUuids, HasFactory, HasEncrypt;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'registration_code',
+        'grade_level',
+        'admission_date',
+        'user_id',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'user_id',
+    ];
+
+    public function casts(): array
+    {
+        return [
+            'registration_code' => 'string',
+            'grade_level' => 'string',
+            'admission_date' => 'date',
+        ];
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2025_04_30_102340_create_students_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->uuid("id")->primary();
+            $table->timestamps();
+            $table->string('registration_code')->unique();
+            $table->string('grade_level');
+            $table->date('admission_date');
+
+            $table->foreignUuid('user_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
