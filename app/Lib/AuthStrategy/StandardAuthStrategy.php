424e0969f139a18a05a6333d86e97ce2fe10ecc9
Cauê Assmann Silva <caueassmannsilva114@gmail.com>
2025-05-17 13:43:09 -0300

    feat: standard logic implementation

---
+++ app/Lib/AuthStrategy/StandardAuthStrategy.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Lib\AuthStrategy;
+
+use App\Lib\LoginStrategy\Interfaces\AuthStrategyInterface;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class StandardAuthStrategy implements AuthStrategyInterface {
+
+    public function __construct(
+        protected UserRepositoryInterface $userRepository
+    ) { }
+    
+    /**
+     * Tenta realizar o login do usuário com as credenciais fornecidas
+     * @param array $credentials
+     * @return bool
+     */
+    public function authenticate(array $credentials): bool
+    {
+        return Auth::attempt($credentials);
+    }
+    
+    /**
+     * Completa o registro do usuário com a senha fornecida
+     * @param User $user
+     * @param string $auth_credential Senha do usuário
+     * @return User
+     */
+    public function makeRegistration(User $user, string $auth_credential): User
+    {
+        $user->password = Hash::make($auth_credential);
+        $this->userRepository->save($user);
+        Auth::login($user);
+        return $user;
+    }
+}
