3e005e3d904b4f4c148cbcd79997197f57842d74
Cauê Assmann Silva <caueassmannsilva114@gmail.com>
2025-06-03 23:08:13 -0300

    feat: web routes names

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Users\Auth\GoogleAuthController;
+use App\Http\Controllers\Users\Auth\StandardAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,11 +9,16 @@ Route::get('/', function () {
 });
 
 Route::prefix('google-auth')->controller(GoogleAuthController::class)->group(function () {
-    Route::get('/generate-login-url', 'generateOAuthLoginUrl');
-    Route::get('/generate-register-url', 'generateOAuthRegisterUrl');
-    Route::get('/generate-regiter-invited-url', 'generateOAuthInvitedUrl');
+    Route::get('/generate-login-url', 'generateOAuthLoginUrl')->name('google.auth.login');
+    Route::get('/generate-register-url', 'generateOAuthRegisterUrl')->name('google.auth.register');
+    Route::get('/generate-regiter-invited-url', 'generateOAuthInvitedUrl')->name('google.auth.register.invited');
 
-    Route::get('/login', 'authenticateCallback');
-    Route::get('/register', 'registerCallback');
-    Route::get('/register-invited', 'registerInvitedUserCallback');
+    Route::get('/login', 'authenticateCallback')->name('google.auth.login.callback');
+    Route::get('/register', 'registerCallback')->name('google.auth.register.callback');
+    Route::get('/register-invited', 'registerInvitedUserCallback')->name('google.auth.register.invited.callback');
+});
+
+Route::prefix('standard-auth')->controller(StandardAuthController::class)->group(function () {
+    Route::post('/register', 'register')->name('auth.register');
+    Route::post('/login', 'authenticate')->name('auth.login');
 });
