e4096ac69eb047dfcac6b00389e1d169c119d38a
Cauê Assmann Silva <caueassmannsilva114@gmail.com>
2025-06-10 22:19:40 -0300

    feat: cors

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,18 @@
 <?php
 
 use App\Http\Controllers\OrganizationController;
-use App\Http\Controllers\Users\RegisterAdminUserController;
+use App\Http\Controllers\Users\Invites\InviteTeachersController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('create-organization', [OrganizationController::class, 'store'])->name('organizations.store');
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware("auth:sanctum")->get('/me', function () {
+    return response()->json(auth()->user());
+})->name('auth.user');
 
-    
-
+Route::middleware("auth:sanctum")->prefix('teachers')->group(function () {
+    Route::post('invite',[InviteTeachersController::class, "store"])->name('users.invite.teachers');
+    Route::post('bulk-invite',[InviteTeachersController::class, "import"])->name('users.invite.teachers');
 });
+
+
